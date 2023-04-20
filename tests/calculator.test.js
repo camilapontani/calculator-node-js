e69294453b2f2b170bf04c8dfad3537c651994b8
Camila Pontani <mila_bets@hotmail.com>
2023-04-20 19:35:00 -0300

    Alteração numeros

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,17 +1,17 @@
 const { calculator } = require("../functions/calculator")
 
-test('Sum test: 1,1', function() {
-    expect(calculator.sum(1,1)).toBe(2);
+test('Sum test: 2,3', function() {
+    expect(calculator.sum(2,3)).toBe(5);
 })
 
-test('Subtract 1,1', function() {
-    expect(calculator.subtract(1,1)).toBe(0);
+test('Subtract 9,2', function() {
+    expect(calculator.subtract(9,2)).toBe(7);
 })
 
-test('Multiply 1,1', function() {
-    expect(calculator.multiply(1,1)).toBe(1);
+test('Multiply 2,4', function() {
+    expect(calculator.multiply(2,4)).toBe(8);
 })
 
-test('Divide 1,1', function() {
-    expect(calculator.divide(1,1)).toBe(1);
+test('Divide 20,4', function() {
+    expect(calculator.divide(20,4)).toBe(5);
 })
